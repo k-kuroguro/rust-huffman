8c226c31f96fe39f4dfe657ff278953d41da4ae0
k-kuroguro <lingjiujinshan@gmail.com>
2022-10-23 02:16:28 +0900

    feat: Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 mod code;
 
+use std::{error, fmt};
+
 use clap::{Parser, Subcommand};
 use code::{decode, encode, Bits};
+
+#[derive(Debug, PartialEq)]
+enum CliError {
+   InvalidPlaneText,
+   InvalidBitString,
+}
+
+impl fmt::Display for CliError {
+   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+      match &self {
+         CliError::InvalidPlaneText => write!(f, "Invalid plane text: detected char except ascii."),
+         CliError::InvalidBitString => {
+            write!(f, "Invalid bit string: detected char except '1' and '0'.")
+         }
+      }
+   }
+}
+
+impl error::Error for CliError {
+   fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+      None
+   }
+}
 
 #[derive(Debug, Parser)]
 #[command(name = "huffman")]
@@ -27,10 +52,18 @@ enum Commands {
    },
 }
 
-fn main() {
+fn handle_error(error: CliError) -> CliError {
+   println!("{}", error);
+   error
+}
+
+fn main() -> Result<(), CliError> {
    let args = Cli::parse();
-   match args.command {
+   Ok(match args.command {
       Commands::Encode { plane } => {
+         if !plane.is_ascii() {
+            return Err(handle_error(CliError::InvalidPlaneText));
+         }
          let code = encode(&plane);
          let bits: String = code
             .into_iter()
@@ -39,15 +72,21 @@ fn main() {
          println!("{}", bits);
       }
       Commands::Decode { bit_string } => {
-         let bits = Bits::from_iter(bit_string.chars().filter(|&x| x == '1' || x == '0').map(
-            |x| match x {
-               '1' => true,
-               '0' => false,
-               _ => unreachable!(),
-            },
-         ));
+         let mut validated_bit_string = bit_string.chars().map(|x| match x {
+            '1' => Ok(true),
+            '0' => Ok(false),
+            _ => Err(CliError::InvalidBitString),
+         });
+         if validated_bit_string.any(|x| x == Err(CliError::InvalidBitString)) {
+            return Err(handle_error(CliError::InvalidBitString));
+         }
+         let bits = Bits::from_iter(bit_string.chars().map(|x| match x {
+            '1' => true,
+            '0' => false,
+            _ => unreachable!(),
+         }));
          let plane = decode(&bits);
          println!("{}", plane);
       }
-   }
+   })
 }
